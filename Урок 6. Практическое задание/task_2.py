b943dfc69f0f0d957a1023a58f01c1b368f0b9a6
Dubkov <yevgeniydubkov@gmail.com>
2021-07-05 16:39:39 +0300

    done

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -4,3 +4,48 @@
 для оптимизации памяти и
 доказать!!! (наглядно, кодом) их эффективность (на примере профилировщика).
 """
+
+from random import choice
+from string import ascii_lowercase
+from pympler import asizeof
+from objgraph import *
+
+"""
+1 - механизм. 
+В примере ниже видно, что string с теми же самыми данными в 8 раз легче, чем list.
+"""
+
+get = [choice(ascii_lowercase) for i in range(100000)]
+get_string = ''.join(get)
+print('Размер list: ', asizeof.asizeof(get))
+print('Размер string: ', asizeof.asizeof(get_string))
+
+"""
+2 - В сложных проектах или при использовании ООП, может быть достаточно полезен модуль objgraph.
+"""
+
+
+class CreateLists:
+    def __init__(self):
+        list_1 = []
+        list_2 = [2, 'fdf']
+
+
+if __name__ == "__main__":
+    a = CreateLists()
+    b = CreateLists()
+
+# Можно узнать сколько экземпляров класса было создано
+print(count('CreateLists'))
+
+# Можно посмотреть какие типы и сколько отслеживает GB
+print(typestats())
+
+# Можно в режиме реального времени смотреть сколько и каких объектов было создано
+
+get_new_ids(limit=0)
+get_new_ids(limit=0)
+a1 = [0, 1, 2]
+b1 = [3, 4, 5]
+c = CreateLists()
+get_new_ids(limit=2)
--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,36 @@
 Придумать как это решить!
 Есть очень простое решение
 """
+
+from memory_profiler import profile
+
+
+@profile
+def factorial(num):
+    def wrap_fact(n):
+        if n <= 1:
+            return n
+        return n * wrap_fact(n - 1)
+    return wrap_fact(num)
+
+
+factorial(100)
+
+
+@profile
+def reverse(num):
+    def wrap_reverse(n):
+        if n == 0:
+            return ''
+        return f'{str(n % 10)}{wrap_reverse(n // 10)}'
+
+    return wrap_reverse(num)
+
+
+reverse(123456)
+
+'''
+Профилировка рекурсивных ф-ций нежелательна, так как профилирование выполняется для каждого вызова функции
+Чтобы избежать срабатывание декоратора при каждом вызове рекурсии необходимо сделать "обертку".
+Поместить функцию с рекурсией в другую функцию.
+'''
